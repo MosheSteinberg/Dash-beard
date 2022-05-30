f6c150e9199761357110edc2e08b6f6ff443675c
Moshe Steinberg <moshe.steinberg@rpc-tyche.com>
2022-05-30 22:19:20 +0100

    try remove / in filepath

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from dash_extensions import BeforeAfter
 app = DashProxy()
 server = app.server
 app.layout = html.Div([
-    BeforeAfter(before="/assets/before.jpg", after="/assets/after.jpg", width=256, height=256, defaultProgress=0.1)
+    BeforeAfter(before="assets/before.jpg", after="assets/after.jpg", width=256, height=256, defaultProgress=0.1)
 ])
 
 if __name__ == "__main__":
