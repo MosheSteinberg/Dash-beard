d563dcd7fe21ed3adacaeaca1b78a03bd5847f71
Moshe Steinberg <moshe.steinberg@rpc-tyche.com>
2022-05-30 22:29:32 +0100

    try using core dash as app

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from dash_extensions.enrich import DashProxy, html
+from dash_extensions.enrich import html
+import dash
 from dash_extensions import BeforeAfter
 
-app = DashProxy()
+app = dash.Dash(__name__)
 server = app.server
 app.layout = html.Div([
     BeforeAfter(before="assets/before.jpg", after="assets/after.jpg", width=256, height=256, defaultProgress=0.1)
