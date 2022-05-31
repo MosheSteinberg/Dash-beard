7a77a6d8cdba540a3acf8e1c0ee05ac2305842c7
Moshe Steinberg <moshe.steinberg@rpc-tyche.com>
2022-05-31 13:33:54 +0100

    Add meta tag for mobiles

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ from dash.html import Div, Button
 from dash_extensions import BeforeAfter
 from dash.dependencies import Input, Output, State
 
-app = Dash(__name__)
+app = Dash(__name__,
+            meta_tags=[{'name': 'viewport',
+                        'content': 'width=device-width, initial-scale=1.0, maximum-scale=1.2, minimum-scale=0.5,'}]
+            )
 server = app.server
 app.title = "Moshe's beard"
 app.layout = Div([
